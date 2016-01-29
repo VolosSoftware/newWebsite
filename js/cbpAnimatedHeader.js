7ec0fdb2a5e3c257f58d9405b1cd9b713f1fce32
TomCorcoran <tom.corcoran13@gmail.com>
2016-01-28 22:49:13 -0500

    New version of the website

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -12,6 +12,10 @@ var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
+		Technology = document.querySelector( '#Technology' ),
+		Firm = document.querySelector( '#Firm' ),
+		Contact = document.querySelector( '#Contact' ),
+		Schedule = document.querySelector( '#Schedule' ),
 		didScroll = false,
 		changeHeaderOn = 300;
 
@@ -28,9 +32,17 @@ var cbpAnimatedHeader = (function() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
 			classie.add( header, 'navbar-shrink' );
+			Technology.style.color = "white";
+			Firm.style.color = "white";
+			Contact.style.color = "white";
+			Schedule.style.color = "white";
 		}
 		else {
 			classie.remove( header, 'navbar-shrink' );
+			Technology.style.color = "black";
+			Firm.style.color = "black";
+			Contact.style.color = "black";
+			Schedule.style.color = "black";
 		}
 		didScroll = false;
 	}
